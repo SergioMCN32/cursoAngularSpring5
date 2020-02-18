87e728e9915213b22ab48e2f6ee9fdcc3b03f3b1
Sergio Herraiz Thomas <sherraiz@atsistemas.com>
2020-02-18 16:23:39 +0100

    Sección 5 completada

--- angular/clientes-app/src/app/clientes/clientes.component.ts
+++ angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
-
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -18,4 +18,32 @@ export class ClientesComponent implements OnInit {
     );
   }
 
+  delete(cliente: Cliente): void {
+    swal({
+      title: '¿Está seguro?',
+      text: `¿Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar',
+      cancelButtonText: 'No, cancelar!',
+      confirmButtonClass: 'btn btn-success',
+      cancelButtonClass: 'btn btn-danger',
+      buttonsStyling: false,
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe(
+          response => {
+            this.clientes = this.clientes.filter(cli => cli !== cliente)
+            swal(
+              'Cliente eliminado!',
+              `Cliente ${cliente.nombre} eliminado con exito.`,
+              'success'
+            );
+      });
+    }
+  });
+}
 }
